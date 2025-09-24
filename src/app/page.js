e8d1ed7d2dbe33e54d32a4b6719f840e66ae16b1
Tagline <tagline@Tagline-6.local>
2025-09-24 12:02:07 +0530

    Added 12 random card demo

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import HeadShow2 from "./components/HeadShow2";
 import HeadShow3 from "./components/HeadShow3";
 import HeadShow4 from "./components/HeadShow4";
 import HeadShow5 from "./components/HeadShow5";
+import ShowCards from "./components/ShowCards";
 
 export default function Home() {
   return (
@@ -253,6 +254,15 @@ export default function Home() {
       <div className="w-full max-w-4xl mt-2">
         <div className="grid grid-cols-2 gap-6">
           <HeadShow5 />
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="w-full max-w-4xl mt-2">
+        <div className="grid grid-cols-2 gap-6">
+          <ShowCards />
         </div>
       </div>
       <br />
